453dfdce2784097574ae4b4b0e1d7218cfd4b095
Sheri Lu <sherisherilu@gmail.com>
2024-10-20 14:49:25 -0700

    Adjust height.

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
     <PageTransition>
       <div className="bg-home-mobile sm:bg-home-tablet xl:bg-home-desktop bg-cover bg-center bg-fixed bg-no-repeat w-full min-h-screen">
         <Header />
-        <main className="flex flex-col xl:flex-row gap-8 justify-between items-center sm:items-start p-5 text-white font-barlow px-10 xl:px-40 xl:py-32">
-          <div className="flex flex-col xl:flex-row xl:pt-72 w-full">
+        <main className="flex flex-col xl:flex-row gap-8 justify-between items-center sm:items-start p-5 text-white font-barlow px-10 xl:px-40 xl:pb-32">
+          <div className="flex flex-col xl:flex-row xl:pt-12 w-full">
             <div className="flex flex-col justify-start items-center xl:items-start xl:justify-end mr-8 w-full xl:w-[600px] pt-28">
               <p className="text-[28px] tracking-[4px] leading-[1.336] text-center text-secondary uppercase font-barlowCondensed">
                 So, you want to travel to
@@ -18,9 +18,9 @@ export default function Home() {
               </h1>
 
               <p className="text-center max-w-xl text-secondary text-[18px] leading-relaxed tracking-norma xl:text-left">
-                Let's face it; if you want to go to space, you might as well
+                Let&apos;s face it; if you want to go to space, you might as well
                 genuinely go to outer space and not hover kind of on the edge of
-                it. Well, sit back, and relax because we'll give you a truly
+                it. Well, sit back, and relax because we&apos;ll give you a truly
                 out-of-this-world experience!
               </p>
             </div>
